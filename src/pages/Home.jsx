4110625c649fe668d839daeac89748ace30d0f5c
JAID-KHAN <jaidkhan04072001@gmail.com>
2026-03-30 21:01:15 +0530

    removed total posts and active jobs section from home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -180,7 +180,7 @@ function Home() {
       </div>
 
       {/* Stats Bar */}
-      <div className="stats-bar">
+      {/* <div className="stats-bar">
         <div className="stat">
           <span className="stat-number">{filteredPosts.length}</span>
           <span className="stat-label">Posts Found</span>
@@ -193,7 +193,7 @@ function Home() {
           <span className="stat-number">{posts.filter(p => p.status === "published").length}</span>
           <span className="stat-label">Published</span>
         </div>
-      </div>
+      </div> */}
 
       {/* Posts Grid */}
       {filteredPosts.length === 0 ? (
